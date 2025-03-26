9c03f5b616dc380a1f6d1f123c2ae21ed71659b7
aravind00 <aravindgohan@gmail.com>
2025-03-26 11:09:05 +0530

    alembic configuration

--- Apis/App/main.py
+++ Apis/App/main.py
@@ -5,19 +5,20 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from .routers import post,user,auth,vote
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-        try:
-                create_db_and_tables()
-                yield
-        except Exception as e:
-                print(f"Error during lifespan: {e}")
-                raise
-        finally:
-                print("Application is shutting down.")
+#alembic
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#         try:
+#                 create_db_and_tables()
+#                 yield
+#         except Exception as e:
+#                 print(f"Error during lifespan: {e}")
+#                 raise
+#         finally:
+#                 print("Application is shutting down.")
 
-app = FastAPI(lifespan=lifespan)
-
+#app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router) 
 app.include_router(auth.router) 

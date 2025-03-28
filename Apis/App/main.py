c031e2f1276ae0570ebfda15de85e94aef416a9f
aravind00 <aravindgohan@gmail.com>
2025-03-28 11:30:05 +0530

    Added CORS

--- Apis/App/main.py
+++ Apis/App/main.py
@@ -4,6 +4,7 @@ from .database import create_db_and_tables
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from .routers import post,user,auth,vote
+from fastapi.middleware.cors import CORSMiddleware
 
 #alembic
 # @asynccontextmanager
@@ -19,6 +20,20 @@ from .routers import post,user,auth,vote
 
 #app = FastAPI(lifespan=lifespan)
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def welcome():
+    return "hello welcome"
+    
 app.include_router(post.router)
 app.include_router(user.router) 
 app.include_router(auth.router) 
